2c0363ae215f6ae8f0c05c7fdd38128eaa2a8a2a
Törőcsik József <ny75m7@inf.elte.hu>
2023-04-11 14:45:49 +0200

    Editing serverItem

--- webapp/src/_components/ServerItem.jsx
+++ webapp/src/_components/ServerItem.jsx
@@ -39,7 +39,10 @@ export default function ServerItem(props){
                 {serverName}
             </div>
             <div className={serverFieldStyle.classes}>
-                {serverStatus}
+                {serverStatus}                
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill={serverStatus=="ON" ? "green" : "red"} class="bi bi-circle-fill" viewBox="0 0 16 16">
+                    <circle cx="8" cy="8" r="8"/>
+                </svg>
             </div>
             <div className={serverFieldStyle.classes}>
                 {serverState}
